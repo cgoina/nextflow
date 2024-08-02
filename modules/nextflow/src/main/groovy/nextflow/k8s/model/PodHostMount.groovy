cf6ea1f1477a420def2fec6c4fd3f60eee601270
Cristian Goina <goinac@hhmi.org>
2024-08-02 19:35:29 -0400

    hostPath type

--- modules/nextflow/src/main/groovy/nextflow/k8s/model/PodHostMount.groovy
+++ modules/nextflow/src/main/groovy/nextflow/k8s/model/PodHostMount.groovy
@@ -30,12 +30,26 @@ import groovy.transform.ToString
 @CompileStatic
 class PodHostMount {
 
+    private static Set<String> VALID_TYPES = [
+        'DirectoryOrCreate', 'Directory', 'FileOrCreate', 'File', 'Socket', 'CharDevice', 'BlockDevice'
+    ]
+
     String hostPath
 
     String mountPath
 
-    PodHostMount(String host, String container) {
+    String type // must be empty or a value from VALID_TYPES
+
+    PodHostMount(String host, String container, String type=null) {
         this.hostPath = host
         this.mountPath = container
+        this.type = type
+        validateTypeEnum(type)
     }
+
+    private static validateTypeEnum(String type) {
+        if( type && !(type in VALID_TYPES) )
+            throw new IllegalArgumentException("K8s invalid hostPath type: $type - must be empty or one of $VALID_TYPES")
+    }
+
 }
